4ff40de875d8af90734289ada55c03dddf7e0c1c
Jinx <chisel.delahanty@gmail.com>
2019-03-25 03:10:11 +0100

    Add achillesHeel property (not yet functional)

--- contract-killer.js
+++ contract-killer.js
@@ -17,42 +17,46 @@
 // Your spy returns, bearing the following information:
 var assassin = {
 	name: "John Q. Smith",
-	blackmailable: false
+	blackmailable: false,
+	achillesHeel: "unknown"
 }
 var client = {
 	name: "Betty Biggins",
-	blackmailable: false
+	blackmailable: false,
+	achillesHeel: "unknown"
 }
 var contractHasBeenSigned = true;
 //=========================================================
 
 
 function saveMyAss(assassin, client) {
-  console.log("running saveMyAss function");
+	console.log("running saveMyAss function");
 	// The client needs to be eliminated in any case.
-  console.log("Attempting to eliminate client.");
+	console.log("Attempting to eliminate client.");
 	var clientEliminated = eliminate(client);
-  if (clientEliminated === true) {
-    console.log("Client eliminated!");
-    console.log("Now let's see about the assassin...");
-  }
+	if (clientEliminated === true) {
+		console.log("Client eliminated!");
+		console.log(client.achillesHeel);
+		console.log("Now let's see about the assassin...");
+	}
 	if (contractHasBeenSigned) {
 		// The assassin's on the job, uh-oh!
-    console.log("Attempting to eliminate assassin.");
+		console.log("Attempting to eliminate assassin.");
 		var assassinEliminated = eliminate(assassin);
-    if (assassinEliminated === true) {
-      console.log("Assassin eliminated!");
-      success();
-    }
+		if (assassinEliminated === true) {
+			console.log("Assassin eliminated!");
+			console.log(assassin.achillesHeel);
+			success();
+		}
 	} else {
-    console.log("Turns out the contract wasn't official yet.");
-    console.log("So no need to worry about the assassin.");
+		console.log("Turns out the contract wasn't official yet.");
+		console.log("So, no need to worry about the assassin.");
 		success();
 	}
 }
 
 function eliminate(target) {
-  var eliminationResults = false;
+	var eliminationResults = false;
 	// This array logs the results of each elimination attempt
 	var attempts = [
 		seduce(target),
@@ -63,32 +67,35 @@ function eliminate(target) {
 	for (let i = 0; i < attempts.length; i++) {
 		if (attempts[i] === true) {
 			eliminationResults = true;
+			break;
 		}
 	}
-  return eliminationResults;
+	return eliminationResults;
 }
 
 function seduce(target) {
-  console.log("Attempting seduction...");
+	console.log("Attempting seduction...");
 	var seduced = false;
 	// You get only one try to seduce them - here we go...
 	var targetWillpower = Math.ceil(Math.random() * 100);
 	var mySexiness = Math.ceil(Math.random() * 100);
 	// Let's see which of you two is more powerful
 	if (mySexiness > targetWillpower) {
+		this.achillesHeel = "Seduction worked!";
 		seduced = true;
 	}
 	return seduced;
 }
 
 function blackmail(target) {
-  console.log("Attempting blackmail...");
+	console.log("Attempting blackmail...");
 	var blackmailed = false;
 	if (this.blackmailable) {
 		// Let's roll a die to see if your spy could get any dirt.
 		var theDirt = Math.ceil(Math.random() * 6);
 		// If the die comes up 5 or 6, the spy succeeded!
 		if (theDirt >= 5) {
+			this.achillesHeel = "Blackmail worked!";
 			blackmailed = true;
 		}
 	}
@@ -96,25 +103,28 @@ function blackmail(target) {
 }
 
 function bribe(target) {
-  console.log("Attempting bribery...");
+	console.log("Attempting bribery...");
 	var bribed = false;
 	// What amount of money would be enough to tempt the target?
 	var targetBreakingPoint = Math.ceil(Math.random() * 10000);
 	// What amount of money do you have in the bank?
 	var cashOnHand = Math.ceil(Math.random() * 10000);
 	if (cashOnHand >= targetBreakingPoint) {
+		this.achillesHeel = "Bribery worked!";
 		bribed = true;
 	}
 	return bribed;
 }
 
 function kill(target) {
-  console.log("Attempting murder...");
+	console.log("Attempting murder...");
 	var dead = false;
-	var opportunity = Math.random();
-	var luck = Math.random();
+  	// Let's see if fate is on your side today.
+  	var opportunity = Math.random();
+  	var luck = Math.random();
 	// If both factors are above 50%, you succeed!
 	if (opportunity > .5 && luck > .5) {
+		this.achillesHeel = "Murder worked!";
 		dead = true;
 	}
 	return dead;
@@ -123,7 +133,7 @@ function kill(target) {
 function success() {
 	console.log("All is well!");
 	console.log("Until you cross somebody else, that is...");
-  return 0;
+	return 0;
 }
 
 function failure() {
@@ -131,9 +141,9 @@ function failure() {
 	console.log("Guess that means it's time to flee the country and change your name.");
 	console.log("Don't act so surprised.");
 	console.log("You knew your evil ways would catch up with you sooner or later.");
-  return 0;
+	return 0;
 }
 
-saveMyAss();
+saveMyAss(assassin, client);
 
 //====================
